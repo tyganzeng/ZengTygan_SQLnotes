e5479b1674dcfec0d661dbbe46b7b057bc2b1a5d
tyganzeng <zengtygan@gmail.com>
2018-05-22 09:00:15 -0700

    DatabaseHelper
    DatabaseHelper added

--- app/src/main/java/com/example/zengt9949/mycontactapp/MainActivity.java
+++ app/src/main/java/com/example/zengt9949/mycontactapp/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.zengt9949.mycontactapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+        Log.d("MyContactApp", "MainActivity: instantiated database");
     }
 }
