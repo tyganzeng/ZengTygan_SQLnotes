1c3390c0a41d0d200e24fd544ddec4908a780449
tyganzeng <zengtygan@gmail.com>
2018-05-22 08:01:00 -0700

    First Commit
    My contact app template

---
+++ app/src/main/java/com/example/zengt9949/mycontactapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zengt9949.mycontactapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
